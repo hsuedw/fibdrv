6d04220faa3fb51fb875a5158c76e5ac3b7ffee4
Edward Hsu <hsuedw@gmail.com>
2022-03-07 18:06:19 +0800

    Implement sysfs interface in Fibonacci driver
    Instead of VFS, use sysfs interface in fibdrv to have a easy interface
    for client to read big Fibonacci numbers.

--- fibdrv.c
+++ fibdrv.c
@@ -19,9 +19,6 @@ MODULE_VERSION("0.1");
  */
 #define MAX_LENGTH 92
 
-static dev_t fib_dev = 0;
-static struct cdev *fib_cdev;
-static struct class *fib_class;
 static DEFINE_MUTEX(fib_mutex);
 
 static long long fib_sequence(long long k)
@@ -39,133 +36,108 @@ static long long fib_sequence(long long k)
     return fk;
 }
 
-static int fib_open(struct inode *inode, struct file *file)
+static int debug_fib_input;
+
+static ssize_t fib_input_show(struct kobject *kobj,
+                              struct kobj_attribute *attr,
+                              char *buf)
 {
-    if (!mutex_trylock(&fib_mutex)) {
-        printk(KERN_ALERT "fibdrv is in use");
-        return -EBUSY;
-    }
-    return 0;
+    return snprintf(buf, 4, "%d\n", debug_fib_input);
 }
 
-static int fib_release(struct inode *inode, struct file *file)
+static ssize_t fib_input_store(struct kobject *kobj,
+                               struct kobj_attribute *attr,
+                               const char *buf,
+                               size_t count)
 {
-    mutex_unlock(&fib_mutex);
-    return 0;
+    int ret;
+
+    ret = kstrtoint(buf, 10, &debug_fib_input);
+    if (ret < 0)
+        return ret;
+
+    return count;
 }
 
-/* calculate the fibonacci number at given offset */
-static ssize_t fib_read(struct file *file,
-                        char *buf,
-                        size_t size,
-                        loff_t *offset)
+/* Sysfs attributes cannot be world-writable. */
+static struct kobj_attribute fib_input_attribute =
+    __ATTR(input,
+           S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP,
+           fib_input_show,
+           fib_input_store);
+
+
+static ssize_t fib_output_show(struct kobject *kobj,
+                               struct kobj_attribute *attr,
+                               char *buf)
 {
-    return (ssize_t) fib_sequence(*offset);
+    long long fk = fib_sequence(debug_fib_input);
+    return snprintf(buf, 21, "%lld\n", fk);
 }
 
-/* write operation is skipped */
-static ssize_t fib_write(struct file *file,
-                         const char *buf,
-                         size_t size,
-                         loff_t *offset)
+/* Sysfs attributes cannot be world-writable. */
+static struct kobj_attribute fib_output_attribute =
+    __ATTR(output, S_IRUSR | S_IRGRP, fib_output_show, NULL);
+
+static ssize_t fib_time_show(struct kobject *kobj,
+                             struct kobj_attribute *attr,
+                             char *buf)
 {
-    return 1;
+    return snprintf(buf, 1, "%d\n", 0);
 }
 
-static loff_t fib_device_lseek(struct file *file, loff_t offset, int orig)
-{
-    loff_t new_pos = 0;
-    switch (orig) {
-    case 0: /* SEEK_SET: */
-        new_pos = offset;
-        break;
-    case 1: /* SEEK_CUR: */
-        new_pos = file->f_pos + offset;
-        break;
-    case 2: /* SEEK_END: */
-        new_pos = MAX_LENGTH - offset;
-        break;
-    }
+/* Sysfs attributes cannot be world-writable. */
+static struct kobj_attribute fib_time_attribute =
+    __ATTR(time, S_IRUSR | S_IRGRP, fib_time_show, NULL);
 
-    if (new_pos > MAX_LENGTH)
-        new_pos = MAX_LENGTH;  // max case
-    if (new_pos < 0)
-        new_pos = 0;        // min case
-    file->f_pos = new_pos;  // This is what we'll use now
-    return new_pos;
-}
-
-const struct file_operations fib_fops = {
-    .owner = THIS_MODULE,
-    .read = fib_read,
-    .write = fib_write,
-    .open = fib_open,
-    .release = fib_release,
-    .llseek = fib_device_lseek,
+/*
+ * Create a group of attributes so that we can create and destroy them all
+ * at once.
+ */
+static struct attribute *attrs[] = {
+    &fib_input_attribute.attr, &fib_output_attribute.attr,
+    &fib_time_attribute.attr,
+    NULL, /* need to NULL terminate the list of attributes */
 };
+
+/*
+ * An unnamed attribute group will put all of the attributes directly in
+ * the kobject directory.  If we specify a name, a subdirectory will be
+ * created for the attributes with the directory being the name of the
+ * attribute group.
+ */
+static struct attribute_group attr_group = {
+    .attrs = attrs,
+};
+
+static struct kobject *fib_kobj;
 
 static int __init init_fib_dev(void)
 {
-    int rc = 0;
+    int retval;
 
     mutex_init(&fib_mutex);
 
-    // Let's register the device
-    // This will dynamically allocate the major number
-    rc = alloc_chrdev_region(&fib_dev, 0, 1, DEV_FIBONACCI_NAME);
+    /*
+     * Create a kobject with the name of "fibonacci",
+     * located under /sys/kernel/
+     */
+    fib_kobj = kobject_create_and_add(DEV_FIBONACCI_NAME, kernel_kobj);
+    if (!fib_kobj)
+        return -ENOMEM;
 
-    if (rc < 0) {
-        printk(KERN_ALERT
-               "Failed to register the fibonacci char device. rc = %i",
-               rc);
-        return rc;
-    }
+    /* Create the files associated with this kobject */
+    retval = sysfs_create_group(fib_kobj, &attr_group);
+    if (retval)
+        kobject_put(fib_kobj);
 
-    fib_cdev = cdev_alloc();
-    if (fib_cdev == NULL) {
-        printk(KERN_ALERT "Failed to alloc cdev");
-        rc = -1;
-        goto failed_cdev;
-    }
-    fib_cdev->ops = &fib_fops;
-    rc = cdev_add(fib_cdev, fib_dev, 1);
-
-    if (rc < 0) {
-        printk(KERN_ALERT "Failed to add cdev");
-        rc = -2;
-        goto failed_cdev;
-    }
-
-    fib_class = class_create(THIS_MODULE, DEV_FIBONACCI_NAME);
-
-    if (!fib_class) {
-        printk(KERN_ALERT "Failed to create device class");
-        rc = -3;
-        goto failed_class_create;
-    }
-
-    if (!device_create(fib_class, NULL, fib_dev, NULL, DEV_FIBONACCI_NAME)) {
-        printk(KERN_ALERT "Failed to create device");
-        rc = -4;
-        goto failed_device_create;
-    }
-    return rc;
-failed_device_create:
-    class_destroy(fib_class);
-failed_class_create:
-    cdev_del(fib_cdev);
-failed_cdev:
-    unregister_chrdev_region(fib_dev, 1);
-    return rc;
+    return retval;
 }
 
 static void __exit exit_fib_dev(void)
 {
     mutex_destroy(&fib_mutex);
-    device_destroy(fib_class, fib_dev);
-    class_destroy(fib_class);
-    cdev_del(fib_cdev);
-    unregister_chrdev_region(fib_dev, 1);
+    kobject_put(fib_kobj);
 }
 
 module_init(init_fib_dev);
